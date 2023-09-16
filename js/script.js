bf4b699da17c6f279f2525a091d03cae44c2bda0
Freedteck <freedteck@gmail.com>
2023-09-16 02:33:43 +0100

    make the calculator work, edit html and add styles

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ function mul(a, b) {
 
 /* function to divide */
 function div(a, b) {
+    if(b === 0) {
+        return 'Huh?😒'
+    }
     return a / b;
 }
 
@@ -24,41 +27,99 @@ function mod(a, b) {
 }
 
 let val1 = 0;
-let val2 = 0;
+let val2 = '';
 let operator = '';
+let result = 0;
 
 /* function to calculate */
 function operate(num1, opera, num2) {
     switch (opera) {
         case '+':
-            add(num1, num2);
-            break;
+            return add(num1, num2);
+
         case '-':
-            sub(num1, num2);
-            break;
+            return sub(num1, num2);
+
         case '*':
-            mul(num1, num2);
-            break;
+            return mul(num1, num2);
+
         case '/':
-            div(num1, num2);
-            break;
+            return div(num1, num2);
+
         case '%':
-            mod(num1, num2);
-            break;
+            return mod(num1, num2);
+
         default:
             return 'ERROR!';
-            break;
+
     }
 }
 
 /* declaring variables */
 const btn = document.querySelectorAll('.btn');
-
+const display = document.getElementById('display')
+let clearScreen = false;
 
 /* adding button functionality */
 btn.forEach(button => {
     const btnValue = button.textContent;
     button.addEventListener('click', () => {
-        console.log(btnValue)
+        if (btnValue === '=') {
+            if (val1 !== '' && val2 !== '') {
+                result = operate(+val1, operator, +val2);
+                display.value = result;
+                console.log(val1);
+                console.log(result);
+                val1 = result;
+                val2 = ''
+            } try {
+                if (!Number.isInteger(result)) {
+                display.value = result.toFixed(11)
+            }
+            } catch (error) {
+                console.log('Out put not a number');
+            }
+            
+        } else if ('+-*/%'.includes(btnValue)) {
+            display.value = val1;
+            clearScreen = true;
+            if (val1 !== '' && val2 !== '') {
+                result = operate(+val1, operator, +val2);
+                console.log(result);
+                val1 = result; 
+                display.value = val1;           
+            }
+            operator = btnValue
+            // val1 = result
+        } else if(btnValue === 'AC') {
+            display.value = '0'
+            val1 = 0
+            val2 = ''
+            operator = ''
+        } else if(btnValue === 'C') {
+            display.value = display.value.slice(0, -1)
+            val1 = display.value
+        } else {
+            if (display.value === '0') {
+                display.value = ''
+            }
+            if(clearScreen) {
+                display.value = ''
+                clearScreen = false
+            }
+            display.value += btnValue;
+
+            if (operator === '') {
+                // If no operator is selected, update val1
+                val1 = display.value;
+            } else {
+                // If an operator is selected, update val2
+                val2 = display.value;
+            }
+        }
+        // else if (btnValue >= 0 && btnValue <= 9 || btnValue === '.') {
+        //     display.value += btnValue;
+        // }
+
     });
 });
